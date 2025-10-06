ae46fdaf60bb2dde26994afdcd05c9baed1f6ef6
Kayleighhhhh <kayleigh.vdham04@gmail.com>
2025-10-06 12:16:13 +0200

    js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JS
+const jaarfilter = document.querySelector(".jaar");
+
+jaarfilter.addEventListener("change", naarfiltered)
+
+function naarfiltered(e) {
+  // ga naar pagina
+  window.location.replace()
+}
