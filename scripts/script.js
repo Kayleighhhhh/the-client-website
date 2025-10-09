f6ef5de02ac8ab1d27bcb1b25dcb05b4dedde5ae
Kayleighhhhh <kayleigh.vdham04@gmail.com>
2025-10-09 09:49:29 +0200

    menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,19 @@
 // JS
 const filter = document.querySelector(".filter");
+const menuBtn = document.querySelector(".navbtn");
+const deHeader = document.querySelector("header");
 
 filter.addEventListener("change", filtered);
+menuBtn.addEventListener("click", toggleMenu);
 
 function filtered(e) {
   // ga naar pagina
   const form = e.target.parentNode;
   form.submit()
 }
+
+function toggleMenu() {
+  //alert("hoi");
+deHeader.classList.toggle("is-open");
+}
+
